9d436041fcd5b05b019b09122486d89a07a13f0b
12301103-SongLiu <12301103@bjtu.edu.cn>
2015-03-29 23:52:51 +0800

    phaseⅡ
    this is a base project of travel alone. just implement two class and few
    layout files

---
+++ TravelAlone/app/src/main/java/com/icesong/travelalone/model/Articles.java
@@ -0,0 +1,20 @@
+package com.icesong.travelalone.model;
+
+import org.w3c.dom.Text;
+
+import java.util.Date;
+
+/**
+ * Created by 12301103 SongLiu(刘嵩) on 3/29.
+ */
+public class Articles {
+    private int mArticleId;     //article NO.
+    private String mArticleName;//article name
+    private String mArticleAuthor;//article author
+    private Date mArticleData; //article written data
+    private String mArticleContent;//'article content
+    private String[] mArticlephotos; //photos used in article
+    private int mArticleLike; //number of people who like this article
+    private int mArticleDislike; //number of people who dislike this article
+    private ArticleComment[] mArticleComments;  //the comments of this article
+}
