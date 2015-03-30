9caac8eade23dfaab1c70503dd3c3408431a3d22
12301107-wangjunmeng <12301107@bjtu.edu.cn>
2015-03-30 17:24:16 +0800

    wangjunmeng
    change

--- TravelAlone/app/src/main/java/com/icesong/travelalone/view/ArticleDetailRead.java
+++ TravelAlone/app/src/main/java/com/icesong/travelalone/view/ArticleDetailRead.java
@@ -6,8 +6,16 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.icesong.travelalone.R;
+
+import org.w3c.dom.Text;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,28 +25,26 @@ import com.icesong.travelalone.R;
 public class ArticleDetailRead extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM1 = "number";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private int number;
+    private MainActivity mainActivity;
+
 
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+
      * @return A new instance of fragment ArticleDetailRead.
      */
     // TODO: Rename and change types and number of parameters
-    public static ArticleDetailRead newInstance(String param1, String param2) {
+    public static ArticleDetailRead newInstance(int number) {
         ArticleDetailRead fragment = new ArticleDetailRead();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_PARAM1, number);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,16 +57,30 @@ public class ArticleDetailRead extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            number = getArguments().getInt(ARG_PARAM1);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_article_detail_read, container, false);
+        View  view = inflater.inflate(R.layout.fragment_article_detail_read, container, false);
+        mainActivity = (MainActivity)getActivity();
+        List<Map<String, Object>> list = mainActivity.getArticleDataList();
+        TextView readTitle = (TextView)view.findViewById(R.id.read_title);
+        TextView readContext = (TextView)view.findViewById(R.id.read_context);
+        ImageView readImage = (ImageView)view.findViewById(R.id.read_image);
+        Map map = (Map)list.get(number);
+
+        Object object = map.get("title");
+        readTitle.setText((String)object);
+
+        Object object1 = map.get("content");
+        readContext.setText((String)object1);
+
+        Object object2 = map.get("img");
+        readImage.setImageResource((int)object2);
+        return view;
     }
 
 
--- TravelAlone/app/src/main/java/com/icesong/travelalone/view/MainActivity.java
+++ TravelAlone/app/src/main/java/com/icesong/travelalone/view/MainActivity.java
@@ -42,8 +42,6 @@ public class MainActivity extends Activity implements ArticleTitleItem.OnFragmen
 
                 new int[]{R.id.article_item_list_title, R.id.article_item_list_detail, R.id.article_item_list_origin,
                         R.id.article_item_list_author, R.id.article_item_list_comments, R.id.imageView});
-
-        articleDataList = new ArrayList<Map<String, Object>>();
     }
 
 
@@ -85,57 +83,37 @@ public class MainActivity extends Activity implements ArticleTitleItem.OnFragmen
 
     @Override
     public void OpenArticleDetailRead(int position) {
-        getFragmentManager().beginTransaction().replace(R.id.fragment_holder, new ArticleDetailRead().newInstance()).addToBackStack(null).commit();
+        getFragmentManager().beginTransaction().replace(R.id.fragment_holder, new ArticleDetailRead().newInstance(position)).addToBackStack(null).commit();
         //getFragmentManager().beginTransaction().replace(R.id.fragment_holder, new ArticleWritten()).addToBackStack(null).commit();
     }
 
-<<<<<<< HEAD
-    public List<Map<String,Object>> getData() {
-        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-=======
-    public List<Map<String, Object>> getArticleDataList() {
->>>>>>> origin/prototype
-
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("title", "article1");
-        map.put("content", "content1");
-        map.put("origin", "origin1");
-        map.put("author", "author1");
-        map.put("comment", "comment1");
-        map.put("img", R.drawable.abc_btn_check_to_on_mtrl_000);
-<<<<<<< HEAD
-        list.add(map);
-=======
-        articleDataList.add(map);
->>>>>>> origin/prototype
-
-        map = new HashMap<String, Object>();
-        map.put("title", "article2");
-        map.put("content", "content2");
-        map.put("origin", "origin2");
-        map.put("author", "author2");
-        map.put("comment", "comment2");
-        map.put("img", R.drawable.abc_btn_radio_material);
-<<<<<<< HEAD
-        list.add(map);
-=======
-        articleDataList.add(map);
->>>>>>> origin/prototype
-
-        map = new HashMap<String, Object>();
-        map.put("title", "article3");
-        map.put("content", "content3");
-        map.put("origin", "origin3");
-        map.put("author", "author3");
-        map.put("comment", "comment3");
-        map.put("img", R.drawable.abc_btn_check_to_on_mtrl_015);
-<<<<<<< HEAD
-        list.add(map);
-
-        return list;
-
-=======
-        articleDataList.add(map);
+    public List<Map<String, Object>> getArticleDataList(){
+//        Map<String, Object> map = new HashMap<String, Object>();
+//        map.put("title", "article1");
+//        map.put("content", "content1");
+//        map.put("origin", "origin1");
+//        map.put("author", "author1");
+//        map.put("comment", "comment1");
+//        map.put("img", R.drawable.abc_btn_check_to_on_mtrl_000);
+//        articleDataList.add(map);
+//
+//        map = new HashMap<String, Object>();
+//        map.put("title", "article2");
+//        map.put("content", "content2");
+//        map.put("origin", "origin2");
+//        map.put("author", "author2");
+//        map.put("comment", "comment2");
+//        map.put("img", R.drawable.abc_btn_radio_material);
+//        articleDataList.add(map);
+//
+//        map = new HashMap<String, Object>();
+//        map.put("title", "article3");
+//        map.put("content", "content3");
+//        map.put("origin", "origin3");
+//        map.put("author", "author3");
+//        map.put("comment", "comment3");
+//        map.put("img", R.drawable.abc_btn_check_to_on_mtrl_015);
+//        articleDataList.add(map);
 
         return articleDataList;
     }
@@ -150,6 +128,5 @@ public class MainActivity extends Activity implements ArticleTitleItem.OnFragmen
         map.put("img", R.drawable.abc_btn_check_to_on_mtrl_000);
 
         articleDataList.add(map);
->>>>>>> origin/prototype
     }
 }
